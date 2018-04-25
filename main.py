97feac8ecd1c04456cbaf58ef8d358b122bd3ed0
antonyjoyce <antonyjoyce@hotmail.com>
2018-04-25 23:09:55 +0100

    test

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-newfile = open("newfile.txt", "w+")
-string = "This is the content that will be written to the text file."
-string2 = "Monkey puzzle."
-newfile.write(string+string2)
+import os
+import simplejson as json
+import django
+
+if os.path.isfile("./ages.json") and os.stat("./ages.json").st_size != 0:
+    old_file = open("./ages.json", "r+")
+    data = json.loads(old_file.read())
+    print("Current age is", data["age"], "-- adding a year")
+    data["age"] = data["age"] + 1
+    print ("New age is", data["age"])
+else:
+    old_file = open("./ages.json", "w+")
+    data = {"name": "Antony", "age": 44}
+    print("No file found, setting default age to", data["age"])
+
+old_file.seek(0)
+old_file.write(json.dumps(data))
